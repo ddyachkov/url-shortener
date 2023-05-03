4e8dd50dcda4b00512ac239d06606fc6ab6a2975
ddyachkov <warlock.kcolraw@gmail.com>
2023-05-03 15:15:12 +0400

    updated app unit tests

--- internal/app/urlshortener_test.go
+++ internal/app/urlshortener_test.go
@@ -2,119 +2,83 @@ package app
 
 import (
 	"context"
-	"fmt"
-	"os"
+	"math/rand"
 	"testing"
 
-	"github.com/ddyachkov/url-shortener/internal/config"
+	"github.com/ddyachkov/url-shortener/internal/random"
 	"github.com/ddyachkov/url-shortener/internal/storage"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
-
-var cfg = config.ServerConfig{
-	FileStoragePath: "/tmp/data.txt",
-	SecretKey:       "thisisthirtytwobytelongsecretkey",
-}
-
-func createFile(t *testing.T, fileStoragePath string, content string) {
-	file, err := os.Create(fileStoragePath)
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		_ = os.Remove(file.Name())
-	})
-	t.Cleanup(func() {
-		_ = file.Close()
-	})
-	_, err = file.Write([]byte(content))
-	require.NoError(t, err)
-}
-
-func Test_makeURI(t *testing.T) {
-	type args struct {
-		id int
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantURI string
-	}{
-		{
-			name:    "Positive_ValidURI",
-			args:    args{id: 1},
-			wantURI: "b",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotURI := makeURI(tt.args.id)
-			assert.Equal(t, gotURI, tt.wantURI)
-		})
-	}
-}
-
-func Test_makeID(t *testing.T) {
-	type args struct {
-		uri string
-	}
-	tests := []struct {
-		name   string
-		args   args
-		wantID int
-	}{
-		{
-			name:   "Positive_ValidID",
-			args:   args{uri: "b"},
-			wantID: 1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotID := makeID(tt.args.uri)
-			assert.Equal(t, gotID, tt.wantID)
-		})
-	}
-}
 
 func TestURLShortener_ReturnURI(t *testing.T) {
 	storage := storage.NewURLMemStorage()
 	service := NewURLShortener(storage)
-	type args struct {
-		url string
-	}
+	url := random.URL().String()
 	tests := []struct {
 		name    string
-		args    args
-		wantURI string
+		url     string
+		wantURI bool
 		wantErr bool
 	}{
 		{
 			name:    "Positive_NewURL",
-			args:    args{url: "https://www.google.ru"},
-			wantURI: "b",
+			url:     url,
+			wantURI: true,
 			wantErr: false,
 		},
 		{
 			name:    "Positive_SameURL",
-			args:    args{url: "https://www.google.ru"},
-			wantURI: "b",
+			url:     url,
+			wantURI: true,
 			wantErr: true,
 		},
 		{
 			name:    "Negative_InvalidURL",
-			args:    args{url: "www.google.ru"},
-			wantURI: "",
+			url:     random.ASCIIString(5, 15),
+			wantURI: false,
 			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotURI, err := service.ReturnURI(context.Background(), tt.args.url, 1)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("URLShortener.ReturnURI() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			assert.Equal(t, gotURI, tt.wantURI)
+			gotURI, err := service.ReturnURI(context.Background(), tt.url, 1)
+			assert.Equal(t, len(gotURI) > 0, tt.wantURI)
+			assert.Equal(t, err != nil, tt.wantErr)
+		})
+	}
+}
+
+func TestURLShortener_ReturnBatchURI(t *testing.T) {
+	storage := storage.NewURLMemStorage()
+	service := NewURLShortener(storage)
+	correctURLs := make([]string, 0)
+	for i := 0; i < 2; i++ {
+		correctURLs = append(correctURLs, random.URL().String())
+	}
+	tests := []struct {
+		name    string
+		urls    []string
+		wantURI bool
+		wantErr bool
+	}{
+		{
+			name:    "Positive_NewURLs",
+			urls:    correctURLs,
+			wantURI: true,
+			wantErr: false,
+		},
+		{
+			name:    "Negative_InvalidURLs",
+			urls:    []string{random.ASCIIString(5, 15)},
+			wantURI: false,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotURI, err := service.ReturnBatchURI(context.Background(), tt.urls, 1)
+			assert.Equal(t, len(gotURI) > 0, tt.wantURI)
+			assert.Equal(t, err != nil, tt.wantErr)
 		})
 	}
 }
@@ -122,83 +86,147 @@ func TestURLShortener_ReturnURI(t *testing.T) {
 func TestURLShortener_GetFullURL(t *testing.T) {
 	storage := storage.NewURLMemStorage()
 	service := NewURLShortener(storage)
-	url := "https://www.google.ru"
-	gotURI, err := service.ReturnURI(context.Background(), url, 1)
+
+	url := random.URL().String()
+	uri, err := service.ReturnURI(context.Background(), url, 1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	type args struct {
-		uri string
-	}
 	tests := []struct {
 		name    string
-		args    args
-		wantURL string
+		uri     string
+		url     string
 		wantErr bool
 	}{
 		{
 			name:    "Positive_URLFound",
-			args:    args{uri: gotURI},
-			wantURL: url,
+			uri:     uri,
+			url:     url,
 			wantErr: false,
 		},
 		{
 			name:    "Negative_URLNotFound",
-			args:    args{uri: "a"},
-			wantURL: "",
+			uri:     random.ASCIIString(5, 15),
+			url:     "",
 			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotURL, err := service.GetFullURL(context.Background(), tt.args.uri)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("URLShortener.GetFullURL() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			assert.Equal(t, gotURL, tt.wantURL)
+			gotURL, err := service.GetFullURL(context.Background(), tt.uri)
+			assert.Equal(t, gotURL, tt.url)
+			assert.Equal(t, err != nil, tt.wantErr)
 		})
 	}
 }
 
-func TestURLShortener_GetFullURL_WithFileStorage(t *testing.T) {
-	url := "https://www.google.ru"
-	dataID := 1
-	userID := 1
-	createFile(t, cfg.FileStoragePath, fmt.Sprint(dataID, " ", url, " ", userID))
-	storage := storage.NewURLFileStorage(&cfg)
+func TestURLShortener_GetUser(t *testing.T) {
+	storage := storage.NewURLMemStorage()
 	service := NewURLShortener(storage)
 
-	type args struct {
-		uri string
-	}
+	userID := rand.Intn(100)
 	tests := []struct {
 		name    string
-		args    args
-		wantURL string
-		wantErr bool
+		newUser bool
 	}{
 		{
-			name:    "Positive_URLFound",
-			args:    args{uri: makeURI(dataID)},
-			wantURL: url,
-			wantErr: false,
+			name:    "Positive_NewUser",
+			newUser: true,
 		},
 		{
-			name:    "Negative_URLNotFound",
-			args:    args{uri: "a"},
-			wantURL: "",
-			wantErr: true,
+			name:    "Positive_SameUser",
+			newUser: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotURL, err := service.GetFullURL(context.Background(), tt.args.uri)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("URLShortener.GetFullURL() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			gotID, _ := service.GetUser(context.Background(), userID)
+			assert.Equal(t, gotID != userID, tt.newUser)
+			if tt.newUser {
+				userID = gotID
 			}
-			assert.Equal(t, gotURL, tt.wantURL)
+		})
+	}
+}
+
+func TestURLShortener_GetURLByUser(t *testing.T) {
+	storage := storage.NewURLMemStorage()
+	service := NewURLShortener(storage)
+
+	userID, err := service.GetUser(context.Background(), 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	urls := make([]string, 0)
+	for i := 0; i < 2; i++ {
+		urls = append(urls, random.URL().String())
+	}
+	_, err = service.ReturnBatchURI(context.Background(), urls, userID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		name     string
+		userID   int
+		wantData bool
+	}{
+		{
+			name:     "Positive_FoundURLs",
+			userID:   userID,
+			wantData: true,
+		},
+		{
+			name:     "Negative_InvalidURLs",
+			userID:   userID + 1,
+			wantData: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotData, _ := service.GetURLByUser(context.Background(), tt.userID)
+			assert.Equal(t, len(gotData) > 0, tt.wantData)
+		})
+	}
+}
+
+func Test_makeURI(t *testing.T) {
+	tests := []struct {
+		name    string
+		id      int
+		wantURI string
+	}{
+		{
+			name:    "Positive_ValidURI",
+			id:      1,
+			wantURI: "b",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotURI := makeURI(tt.id)
+			assert.Equal(t, gotURI, tt.wantURI)
+		})
+	}
+}
+
+func Test_makeID(t *testing.T) {
+	tests := []struct {
+		name   string
+		uri    string
+		wantID int
+	}{
+		{
+			name:   "Positive_ValidID",
+			uri:    "b",
+			wantID: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotID := makeID(tt.uri)
+			assert.Equal(t, gotID, tt.wantID)
 		})
 	}
 }
