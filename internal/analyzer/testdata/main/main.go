c9062294be70c3ddf947b792ed833bb217903b6e
ddyachkov <warlock.kcolraw@gmail.com>
2023-05-17 09:20:44 +0400

    added complexity to test data

--- internal/analyzer/testdata/main/main.go
+++ internal/analyzer/testdata/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 )
 
@@ -13,5 +14,16 @@ func osExit(code int) {
 func main() {
 	fmt.Println("this is main function")
 	osExit(1)
-	os.Exit(1) // want "cannot call os.Exit in function main in package main"
+	if 2 > 1 {
+		for 1 > 0 {
+			r := rand.Intn(10)
+			switch r {
+			case 5:
+				fmt.Println("hit")
+				os.Exit(1) // want "cannot call os.Exit in function main in package main"
+			default:
+				fmt.Println("miss")
+			}
+		}
+	}
 }
